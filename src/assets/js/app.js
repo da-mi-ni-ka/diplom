df7f7bb7ff3faa79172105af0a5b762517b26b7d
da-mi-ni-ka <daminika.trafimchyk@gmail.com>
2021-04-21 17:31:47 +0300

    swiper

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,8 +2,6 @@ $(function () {
     'use strict';
     
     const swiper = new Swiper('.swiper-container', {
-        // Optional parameters
-        // direction: 'vertical',
         loop: true,
         slidesPerView: 3,
         slidesPerColumn: 2,
@@ -11,25 +9,63 @@ $(function () {
         slidesPerGroup: 1,
         spaceBetween: 10,
         grabCursor: true,
-    
-        
-      
-        // If we need pagination
+
         pagination: {
           el: '.swiper-pagination',
           clickable: true,
           dynamicBullets: true,
         },
       
-        // Navigation arrows
         navigation: {
           nextEl: '.swiper-button-next',
           prevEl: '.swiper-button-prev',
         },
       
-        // And if we need scrollbar
-        // scrollbar: {
-        //   el: '.swiper-scrollbar',
-        // },
       });
+});
+
+$(function () {
+  'use strict';
+  
+  const swiper = new Swiper('.swiper-container-works', {
+      loop: true,
+      slidesPerView: 4,
+      slidesPerColumn: 2,
+      slidesPerColumnFill: 'row',
+      slidesPerGroup: 1,
+      spaceBetween: 10,
+      
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+      },
+
+      navigation: {
+        nextEl: '.swiper-button-next.btn-works-r',
+        prevEl: '.swiper-button-prev.btn-works-l',
+      },
+    
+    });
+});
+
+$(function () {
+  'use strict';
+  
+  const swiper = new Swiper('.swiper-container-thanks', {
+      loop: true,
+      slidesPerView: 3,
+      
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+      },
+      
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    
+    });
 });
