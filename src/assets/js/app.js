7f260b21e6d6fe6025b56f8b025e0b06e77b9001
da-mi-ni-ka <daminika.trafimchyk@gmail.com>
2021-04-22 22:34:35 +0300

    swiper-changed

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,7 +1,7 @@
 $(function () {
     'use strict';
     
-    const swiper = new Swiper('.swiper-container', {
+    const swiper1 = new Swiper('.swiper-container-etching', {
         loop: true,
         slidesPerView: 3,
         slidesPerColumn: 2,
@@ -11,61 +11,45 @@ $(function () {
         grabCursor: true,
 
         pagination: {
-          el: '.swiper-pagination',
+          el: '.swiper-pagination-etching',
           clickable: true,
           dynamicBullets: true,
         },
       
         navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+          nextEl: '.swiper-button-next-etching',
+          prevEl: '.swiper-button-prev-etching',
         },
       
       });
-});
-
-$(function () {
-  'use strict';
-  
-  const swiper = new Swiper('.swiper-container-works', {
+    const swiper2 = new Swiper('.swiper-container-works', {
       loop: true,
       slidesPerView: 4,
       slidesPerColumn: 2,
       slidesPerColumnFill: 'row',
       slidesPerGroup: 1,
       spaceBetween: 10,
-      
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        dynamicBullets: true,
-      },
+    
 
       navigation: {
         nextEl: '.swiper-button-next.btn-works-r',
         prevEl: '.swiper-button-prev.btn-works-l',
       },
-    
     });
-});
-
-$(function () {
-  'use strict';
+  const swiper3 = new Swiper('.swiper-container-thanks', {
+    loop: true,
+    slidesPerView: 3,
+    
+    pagination: {
+      el: '.swiper-pagination-thanks',
+      clickable: true,
+      dynamicBullets: true,
+    },
+    
+    navigation: {
+      nextEl: '.swiper-button-next-thanks',
+      prevEl: '.swiper-button-prev-thanks',
+    },
   
-  const swiper = new Swiper('.swiper-container-thanks', {
-      loop: true,
-      slidesPerView: 3,
-      
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        dynamicBullets: true,
-      },
-      
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    
-    });
+  });
 });
